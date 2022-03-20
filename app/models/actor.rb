1d70be437795dcbbc50ec691f560794673b3260f
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-03-20 18:00:35 -0400

    commit tree ff1110ebb6173ee11147a81b9d958fb162841d14

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    roles_array = []
+    self.characters.each {|char| roles_array << "#{char.name} - #{char.show.name}"}
+    roles_array
+  end
   
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  belongs_to :network
+  has_many :actors, through: :characters
+
+  def actors_list
+    actors_array = []
+    self.actors.each {|actor| actors_array << actor.full_name}
+    actors_array
+  end
   
 end
